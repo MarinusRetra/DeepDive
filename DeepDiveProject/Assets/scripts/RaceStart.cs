930aabba6c74af6768f84cdec2ca277f1d40e9fd
Marinus Retra <marinus@flowonline.nl>
2024-05-14 13:58:00 +0200

    FinishLine
    + Finish Line Model
    + Countdown Voor Race Start

---
+++ DeepDiveProject/Assets/scripts/RaceStart.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class RaceStart : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(nameof(OnRaceStart));
+    }
+
+
+    IEnumerator OnRaceStart()
+    { 
+      for (int i = 0; i < gameObject.transform.childCount; i++)
+      { 
+          yield return new WaitForSeconds(1);
+          gameObject.transform.GetChild(i).GetChild(0).GetComponent<Light>().intensity = 1;
+          gameObject.transform.GetChild(i).GetComponent<MeshRenderer>().material.color = Color.red;
+      }
+
+      yield return new WaitForSeconds(Random.Range(0.1f, 3.01f));
+
+      for (int i = 0; i < gameObject.transform.childCount; i++)
+      {
+            gameObject.transform.GetChild(i).GetChild(0).GetComponent<Light>().intensity = 0;
+            gameObject.transform.GetChild(i).GetComponent<MeshRenderer>().material.color = Color.black;
+        }
+    }
+}
